98de045f79abfac3cf56ac20837506ae637b02d7
quat <scratch933@gmail.com>
2018-11-04 01:09:16 -0400

    PRESHOW: THE WORKSPACE SETUP

---
+++ src/main/java/quaternary/superbalancedmodmultiblockfurnace2theempiresmeltsback/SuperBalancedModMultiblockFurnace2TheEmpireSmeltsBack.java
@@ -0,0 +1,14 @@
+package quaternary.superbalancedmodmultiblockfurnace2theempiresmeltsback;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+				modid = SuperBalancedModMultiblockFurnace2TheEmpireSmeltsBack.MODID,
+				name = SuperBalancedModMultiblockFurnace2TheEmpireSmeltsBack.NAME,
+				version = SuperBalancedModMultiblockFurnace2TheEmpireSmeltsBack.VERSION
+)
+public class SuperBalancedModMultiblockFurnace2TheEmpireSmeltsBack {
+	public static final String MODID = "superbalancedmodmultiblockfurnace2theempiresmeltsback";
+	public static final String NAME = "Super Balanced Mod Multiblock Furnace 2: The Empire Smelts Back";
+	public static final String VERSION = "GRADLE:VERSION";
+}
